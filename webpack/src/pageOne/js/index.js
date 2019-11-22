21f36f2bffa8c8629cc2dfd0aeb087c0220a4afd
dasha <frgyvu@gmail.com>
2019-11-23 00:21:45 +0300

    ++

---
+++ webpack/src/pageOne/js/index.js
@@ -0,0 +1,7 @@
+import {add} from "./other";
+// import "../css/style.css";
+import "../scss/style.scss";
+
+
+let res = add(10, 7);
+console.log("Page One: ", res);
---
+++ webpack/webpack.config.js
@@ -0,0 +1,65 @@
+// webpack.config.js
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+
+    entry: {
+        pageOne: './src/pageOne/js/index.js',
+        pageTwo: './src/pageTwo/js/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env"],
+                            plugins: ["@babel/plugin-transform-runtime"]
+                        }
+                    }
+                ]
+            },
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         {
+            //             loader: MiniCssExtractPlugin.loader,
+            //             options: {}
+            //         },
+            //         'css-loader'
+            //     ]
+            // }
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            }
+
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+
+};
